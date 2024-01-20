0df443f1a486a9e8441588df788a9326993e8831
Vlad-Eris <vlad.mihai@emanuel.ro>
2024-01-20 13:37:35 +0200

    Impartire 2 numere

---
+++ Impartirea-a-doua-numere/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Impartirea_a_doua_numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = x / y;
+
+            if (x != 0 && x % y == 0)
+            {
+                Console.WriteLine(z);
+            }
+            else
+            {
+                Console.WriteLine("Impartirea nu poate fi efectuata");
+            }
+        }
+    }
+}
